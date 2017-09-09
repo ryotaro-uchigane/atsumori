ebd0882a9141007b486a3f5792c7a4f1a2fdab55
kichinaga <pixo.3303@gmail.com>
2017-09-10 02:47:02 +0900

    eventモデルのvalidationを追記

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
   has_many :documents
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :title, presence: true
+  validates :description,
+  validates :public, inclusion: { in: [true, false] }
 end

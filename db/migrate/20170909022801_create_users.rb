85abc0f13895b0f9b6507a510978e0d911bb6e90
kichinaga <pixo.3303@gmail.com>
2017-09-09 14:20:23 +0900

    アロー演算子を使っていた部分を修正

--- db/migrate/20170909022801_create_users.rb
+++ db/migrate/20170909022801_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.integer :entrysheet_id
-      t.text :email, :null => false
-      t.text :password, :null => false
-      t.text :role, :null => false
+      t.text :email, null: false
+      t.text :password, null: false
+      t.text :role, null: false
 
       t.timestamps
     end

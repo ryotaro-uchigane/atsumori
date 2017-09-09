41427437e35e9d1e4520fd71e5e2152bc5d507dd
kichinaga <pixo.3303@gmail.com>
2017-09-10 00:17:07 +0900

    編集機能と新規作成機能を作成 ログイン情報（user_id）をsessionで保持するように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,39 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
   def show
     @user = User.find_by(id: params[:id])
   end
 
   def edit
+    @user = User.find_by(id: params[:id])
   end
 
   def update
+    @user = User.find_by(id: params[:id])
+
+    @user.update(user_params)
+
+    redirect_to action: :show, id: @user.id
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+
+    redirect_to action: :show, id: @user.id
+  end
+
+  def destroy
+    User.find_by(id: params[:id]).destroy
+    redirect_to '/login'
   end
 
   def login_form
@@ -17,11 +44,23 @@ class UsersController < ApplicationController
 
     if @user
       flash[:notice] = 'ログインしました'
-      redirect_to("/users/#{@user.id}")
+      session[:user_id] = @user.id
+      redirect_to action: :index
     else
       @error_message = 'メールアドレスまたはパスワードが間違っています'
       @email = params[:email]
       render('users/login_form')
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to action: :index
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :role)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
+
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  get "logout" => "users#logout"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

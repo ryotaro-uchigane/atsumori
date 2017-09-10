932b6388e4c322d2f9f652ce3aa85c23641dfb03
inoue <nxt.gjt9951@gmail.com>
2017-09-11 01:15:20 +0900

    企業側のチャットの関数化

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -24,33 +24,35 @@ function getMedia(video) {
 }
 
 
-peer = new Peer({
-  key: "058778ca-19fc-49dc-b554-68b0054c358c",
-  debug: 3
-});
+function setCompanyChats() {
+  peer = new Peer({
+    key: "058778ca-19fc-49dc-b554-68b0054c358c",
+    debug: 3
+  });
 
-peer.on('open', function() {
-  console.log("ok");
-  room = setRoom(localStream);
-});
+  peer.on('open', function() {
+    console.log("ok");
+    room = setRoom(localStream);
+  });
 
-function setRoom(newStream) {
-  room = peer.joinRoom('test-room');
-  room.on('open', function() {
-    console.log("room open");
-    this.replaceStream(newStream);
-  });
-  room.on('peerJoin', function() {
-    console.log("peer Join!");
-    this.replaceStream(newStream);
-  });
-  room.on('stream', function(stream) {
-    console.log("recive stream");
-    console.log(stream);
-    let atherVideo = document.getElementById('athor-video');
-    atherVideo.src = window.URL.createObjectURL(stream);
-  });
-  room.on('peerLeave', function() {
-    console.log("peer Leave!");
-  });
+  function setRoom(newStream) {
+    room = peer.joinRoom('test-room');
+    room.on('open', function() {
+      console.log("room open");
+      this.replaceStream(newStream);
+    });
+    room.on('peerJoin', function() {
+      console.log("peer Join!");
+      this.replaceStream(newStream);
+    });
+    room.on('stream', function(stream) {
+      console.log("recive stream");
+      console.log(stream);
+      let atherVideo = document.getElementById('athor-video');
+      atherVideo.src = window.URL.createObjectURL(stream);
+    });
+    room.on('peerLeave', function() {
+      console.log("peer Leave!");
+    });
+  }
 }
